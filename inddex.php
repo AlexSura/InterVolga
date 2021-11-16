8484e54dfd3c20725a92903d31aca2fa753f795b
AlexM~D <surin.av@mail.ru>
2021-11-16 12:47:21 +0500

    InterVolga

---
+++ inddex.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  </head>
+<body>
+  <h1>Процесс «Chromium host executable» или отвечает за связь с серверами Autodesk. Это необходимо для обновления продукта,
+  получения справки и доступа к облачному хранилищу. Возможно, происходит сбой в передачи данных, который ведет к перегрузке ЦП.";</h1>
+</body>
+</html>
